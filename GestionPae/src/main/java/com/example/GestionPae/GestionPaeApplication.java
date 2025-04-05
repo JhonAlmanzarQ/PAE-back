0aef11af1a33a51031b4d00d0ee74289c9a572ac
JhonAlmanzarQ <jhonalmanzar003@gmail.com>
2025-04-04 23:41:41 -0500

    Creación del Proyecto

---
+++ GestionPae/src/main/java/com/example/GestionPae/GestionPaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionPae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPaeApplication.class, args);
+	}
+
+}
---
+++ GestionPae/src/test/java/com/example/GestionPae/GestionPaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionPae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
